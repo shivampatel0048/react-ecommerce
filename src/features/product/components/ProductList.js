2d52da3b2e324f9959b39952c1e7ce68d8ab54c7
Shivam Patel <shivampatek0048@gmail.com>
2023-10-24 17:15:30 +0530

    Brands and Category API Setup Completed

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useState, Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchAllProductsAsync, fetchAllProductsByFiltersAsync, selectAllProducts, selectTotalItems } from '../productSlice';
+import { fetchAllProductsAsync, fetchAllProductsByFiltersAsync, fetchBrandsAsync, fetchCategoriesAsync, selectAllProducts, selectBrands, selectCategories, selectTotalItems } from '../productSlice';
 import { ChevronLeftIcon, ChevronRightIcon, StarIcon } from '@heroicons/react/20/solid'
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
@@ -15,305 +15,29 @@ const sortOptions = [
   { name: 'Price: Low to High', sort: 'price', order: 'asc', current: false },
   { name: 'Price: High to Low', sort: 'price', order: 'desc', current: false },
 ]
-const filters = [
-  {
-    id: 'category',
-    name: 'Category',
-    options: [
-      // { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-      // { value: 'sale', label: 'Sale', checked: false },
-      // { value: 'travel', label: 'Travel', checked: false },
-      // { value: 'organization', label: 'Organization', checked: false },
-      // { value: 'accessories', label: 'Accessories', checked: false },
-      { value: 'smartphones', label: 'smartphones', checked: false },
-      { value: 'laptops', label: 'laptops', checked: false },
-      { value: 'fragrances', label: 'fragrances', checked: false },
-      { value: 'skincare', label: 'skincare', checked: false },
-      { value: 'groceries', label: 'groceries', checked: false },
-      {
-        value: 'home-decoration',
-        label: 'home,decoration',
-        checked: false
-      },
-      { value: 'furniture', label: 'furniture', checked: false },
-      { value: 'tops', label: 'tops', checked: false },
-      {
-        value: 'womens-dresses',
-        label: 'womens,dresses',
-        checked: false
-      },
-      { value: 'womens-shoes', label: 'womens,shoes', checked: false },
-      { value: 'mens-shirts', label: 'mens,shirts', checked: false },
-      { value: 'mens-shoes', label: 'mens,shoes', checked: false },
-      { value: 'mens-watches', label: 'mens,watches', checked: false },
-      {
-        value: 'womens-watches',
-        label: 'womens,watches',
-        checked: false
-      },
-      { value: 'womens-bags', label: 'womens,bags', checked: false },
-      {
-        value: 'womens-jewellery',
-        label: 'womens,jewellery',
-        checked: false
-      },
-      { value: 'sunglasses', label: 'sunglasses', checked: false },
-      { value: 'automotive', label: 'automotive', checked: false },
-      { value: 'motorcycle', label: 'motorcycle', checked: false },
-      { value: 'lighting', label: 'lighting', checked: false }
 
-    ],
-  },
-  {
-    id: 'brand',
-    name: 'Brands',
-    options: [
-      { value: 'Apple', label: 'Apple', checked: false },
-      { value: 'Samsung', label: 'Samsung', checked: false },
-      { value: 'OPPO', label: 'OPPO', checked: false },
-      { value: 'Huawei', label: 'Huawei', checked: false },
-      {
-        value: 'Microsoft Surface',
-        label: 'Microsoft Surface',
-        checked: false
-      },
-      { value: 'Infinix', label: 'Infinix', checked: false },
-      { value: 'HP Pavilion', label: 'HP Pavilion', checked: false },
-      {
-        value: 'Impression of Acqua Di Gio',
-        label: 'Impression of Acqua Di Gio',
-        checked: false
-      },
-      { value: 'Royal_Mirage', label: 'Royal_Mirage', checked: false },
-      {
-        value: 'Fog Scent Xpressio',
-        label: 'Fog Scent Xpressio',
-        checked: false
-      },
-      { value: 'Al Munakh', label: 'Al Munakh', checked: false },
-      {
-        value: 'Lord - Al-Rehab',
-        label: 'Lord , Al,Rehab',
-        checked: false
-      },
-      {
-        value: 'L\'Oreal Paris',
-        label: 'L\'Oreal Paris',
-        checked: false
-      },
-      { value: 'Hemani Tea', label: 'Hemani Tea', checked: false },
-      { value: 'Dermive', label: 'Dermive', checked: false },
-      {
-        value: 'ROREC White Rice',
-        label: 'ROREC White Rice',
-        checked: false
-      },
-      { value: 'Fair & Clear', label: 'Fair & Clear', checked: false },
-      { value: 'Saaf & Khaas', label: 'Saaf & Khaas', checked: false },
-      {
-        value: 'Bake Parlor Big',
-        label: 'Bake Parlor Big',
-        checked: false
-      },
-      {
-        value: 'Baking Food Items',
-        label: 'Baking Food Items',
-        checked: false
-      },
-      { value: 'fauji', label: 'fauji', checked: false },
-      { value: 'Dry Rose', label: 'Dry Rose', checked: false },
-      { value: 'Boho Decor', label: 'Boho Decor', checked: false },
-      {
-        value: 'Flying Wooden',
-        label: 'Flying Wooden',
-        checked: false
-      },
-      { value: 'LED Lights', label: 'LED Lights', checked: false },
-      {
-        value: 'luxury palace',
-        label: 'luxury palace',
-        checked: false
-      },
-      { value: 'Golden', label: 'Golden', checked: false },
-      {
-        value: 'Furniture Bed Set',
-        label: 'Furniture Bed Set',
-        checked: false
-      },
-      {
-        value: 'Ratttan Outdoor',
-        label: 'Ratttan Outdoor',
-        checked: false
-      },
-      {
-        value: 'Kitchen Shelf',
-        label: 'Kitchen Shelf',
-        checked: false
-      },
-      {
-        value: 'Multi Purpose',
-        label: 'Multi Purpose',
-        checked: false
-      },
-      { value: 'AmnaMart', label: 'AmnaMart', checked: false },
-      {
-        value: 'Professional Wear',
-        label: 'Professional Wear',
-        checked: false
-      },
-      { value: 'Soft Cotton', label: 'Soft Cotton', checked: false },
-      { value: 'Top Sweater', label: 'Top Sweater', checked: false },
-      {
-        value: 'RED MICKY MOUSE..',
-        label: 'RED MICKY MOUSE..',
-        checked: false
-      },
-      {
-        value: 'Digital Printed',
-        label: 'Digital Printed',
-        checked: false
-      },
-      { value: 'Ghazi Fabric', label: 'Ghazi Fabric', checked: false },
-      { value: 'IELGY', label: 'IELGY', checked: false },
-      {
-        value: 'IELGY fashion',
-        label: 'IELGY fashion',
-        checked: false
-      },
-      {
-        value: 'Synthetic Leather',
-        label: 'Synthetic Leather',
-        checked: false
-      },
-      {
-        value: 'Sandals Flip Flops',
-        label: 'Sandals Flip Flops',
-        checked: false
-      },
-      {
-        value: 'Maasai Sandals',
-        label: 'Maasai Sandals',
-        checked: false
-      },
-      {
-        value: 'Arrivals Genuine',
-        label: 'Arrivals Genuine',
-        checked: false
-      },
-      {
-        value: 'Vintage Apparel',
-        label: 'Vintage Apparel',
-        checked: false
-      },
-      { value: 'FREE FIRE', label: 'FREE FIRE', checked: false },
-      {
-        value: 'The Warehouse',
-        label: 'The Warehouse',
-        checked: false
-      },
-      { value: 'Sneakers', label: 'Sneakers', checked: false },
-      { value: 'Rubber', label: 'Rubber', checked: false },
-      { value: 'Naviforce', label: 'Naviforce', checked: false },
-      { value: 'SKMEI 9117', label: 'SKMEI 9117', checked: false },
-      {
-        value: 'Strap Skeleton',
-        label: 'Strap Skeleton',
-        checked: false
-      },
-      { value: 'Stainless', label: 'Stainless', checked: false },
-      {
-        value: 'Eastern Watches',
-        label: 'Eastern Watches',
-        checked: false
-      },
-      {
-        value: 'Luxury Digital',
-        label: 'Luxury Digital',
-        checked: false
-      },
-      { value: 'Watch Pearls', label: 'Watch Pearls', checked: false },
-      { value: 'Bracelet', label: 'Bracelet', checked: false },
-      { value: 'LouisWill', label: 'LouisWill', checked: false },
-      {
-        value: 'Copenhagen Luxe',
-        label: 'Copenhagen Luxe',
-        checked: false
-      },
-      { value: 'Steal Frame', label: 'Steal Frame', checked: false },
-      { value: 'Darojay', label: 'Darojay', checked: false },
-      {
-        value: 'Fashion Jewellery',
-        label: 'Fashion Jewellery',
-        checked: false
-      },
-      {
-        value: 'Cuff Butterfly',
-        label: 'Cuff Butterfly',
-        checked: false
-      },
-      {
-        value: 'Designer Sun Glasses',
-        label: 'Designer Sun Glasses',
-        checked: false
-      },
-      { value: 'mastar watch', label: 'mastar watch', checked: false },
-      { value: 'Car Aux', label: 'Car Aux', checked: false },
-      { value: 'W1209 DC12V', label: 'W1209 DC12V', checked: false },
-      { value: 'TC Reusable', label: 'TC Reusable', checked: false },
-      {
-        value: 'Neon LED Light',
-        label: 'Neon LED Light',
-        checked: false
-      },
-      {
-        value: 'METRO 70cc Motorcycle - MR70',
-        label: 'METRO 70cc Motorcycle , MR70',
-        checked: false
-      },
-      { value: 'BRAVE BULL', label: 'BRAVE BULL', checked: false },
-      {
-        value: 'shock absorber',
-        label: 'shock absorber',
-        checked: false
-      },
-      { value: 'JIEPOLLY', label: 'JIEPOLLY', checked: false },
-      { value: 'Xiangle', label: 'Xiangle', checked: false },
-      {
-        value: 'lightingbrilliance',
-        label: 'lightingbrilliance',
-        checked: false
-      },
-      { value: 'Ifei Home', label: 'Ifei Home', checked: false },
-      { value: 'DADAWU', label: 'DADAWU', checked: false },
-      { value: 'YIOSI', label: 'YIOSI', checked: false }
-
-    ],
-  },
-  /* {
-  //   id: 'size',
-  //   name: 'Size',
-  //   options: [
-  //     { value: '2l', label: '2L', checked: false },
-  //     { value: '6l', label: '6L', checked: false },
-  //     { value: '12l', label: '12L', checked: false },
-  //     { value: '18l', label: '18L', checked: false },
-  //     { value: '20l', label: '20L', checked: false },
-  //     { value: '40l', label: '40L', checked: false },
-  //   ],
-   },*/
-]
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-
-
-
-
 export default function ProductList() {
   const dispatch = useDispatch();
   const products = useSelector(selectAllProducts);
+  const brands = useSelector(selectBrands);
+  const categories = useSelector(selectCategories);
   const totalItems = useSelector(selectTotalItems);
+  const filters = [
+    {
+      id: 'category',
+      name: 'Category',
+      options: categories.data,
+    },
+    {
+      id: 'brand',
+      name: 'Brands',
+      options: brands.data,
+    },
+  ]
   const [filter, setFilter] = useState({});
   const [sort, setSort] = useState({});
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
@@ -332,9 +56,7 @@ export default function ProductList() {
       const index = newFilter[section.id].findIndex(el => el === option.value);
       newFilter[section.id].splice(index, 1);
     }
-    console.log({ newFilter })
     setFilter(newFilter);
-    // console.log(section.id, option.value);
   }
 
   const handleSort = (e, option) => {
@@ -344,7 +66,6 @@ export default function ProductList() {
   }
 
   const handlePage = (page) => {
-    console.log({ page });
     setPage(page);
   }
 
@@ -357,6 +78,11 @@ export default function ProductList() {
     setPage(1);
   }, [totalItems, sort])
 
+  useEffect(() => {
+    dispatch(fetchBrandsAsync());
+    dispatch(fetchCategoriesAsync());
+  }, [])
+
   return (
     <div>
       <div>
@@ -364,7 +90,7 @@ export default function ProductList() {
         <div className="bg-white">
           <div>
             {/* Mobile filter dialog  */}
-            <MobileFilter handleFilter={handleFilter} mobileFiltersOpen={mobileFiltersOpen} setMobileFiltersOpen={setMobileFiltersOpen}></MobileFilter>
+            <MobileFilter handleFilter={handleFilter} filters={filters} mobileFiltersOpen={mobileFiltersOpen} setMobileFiltersOpen={setMobileFiltersOpen}></MobileFilter>
 
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -436,7 +162,7 @@ export default function ProductList() {
                 </h2>
                 <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
                   {/* Filters */}
-                  <DesktopFilter handleFilter={handleFilter}></DesktopFilter>
+                  <DesktopFilter handleFilter={handleFilter} filters={filters}></DesktopFilter>
                   {/* Product grid */}
                   <div className="lg:col-span-3">
                     {/* This is our products list */}
@@ -457,7 +183,7 @@ export default function ProductList() {
 }
 
 
-function MobileFilter({ mobileFiltersOpen, setMobileFiltersOpen, handleFilter }) {
+function MobileFilter({ mobileFiltersOpen, setMobileFiltersOpen, handleFilter, filters }) {
   return (
     <Transition.Root show={mobileFiltersOpen} as={Fragment}>
       <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
@@ -552,7 +278,7 @@ function MobileFilter({ mobileFiltersOpen, setMobileFiltersOpen, handleFilter })
   );
 }
 
-function DesktopFilter({ handleFilter }) {
+function DesktopFilter({ handleFilter, filters }) {
   return (
     <form className="hidden lg:block">
       {filters.map((section) => (
@@ -573,7 +299,7 @@ function DesktopFilter({ handleFilter }) {
               </h3>
               <Disclosure.Panel className="pt-6">
                 <div className="space-y-4">
-                  {section.options.map((option, optionIdx) => (
+                  {Array.isArray(section.options) && section.options.map((option, optionIdx) => (
                     <div key={option.value} className="flex items-center">
                       <input
                         id={`filter-${section.id}-${optionIdx}`}
@@ -603,21 +329,22 @@ function DesktopFilter({ handleFilter }) {
 }
 
 function Pagination({ page, setPage, handlePage, totalItems }) {
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          thumbnail="#"
+        <div
+          onClick={(e) => handlePage(page > 1 ? page - 1 : page)}
           className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Previous
-        </a>
-        <a
-          thumbnail="#"
+        </div>
+        <div
+          onClick={(e) => handlePage(page < totalPages ? page + 1 : page)}
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Next
-        </a>
+        </div>
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
@@ -628,18 +355,18 @@ function Pagination({ page, setPage, handlePage, totalItems }) {
         </div>
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <a
-              thumbnail="#"
+            <div
+              onClick={(e) => handlePage(page > 1 ? page - 1 : page)}
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
 
-            {Array.from({ length: Math.ceil(totalItems / ITEMS_PER_PAGE) }).map((el, index) => (
+            {Array.from({ length: totalPages }).map((el, index) => (
               <div
-                onClick={(e) => handlePage(index + 1)}
+                onClick={(e) => handlePage(page < totalPages ? page + 1 : page)}
                 thumbnail="#"
                 aria-current="page"
                 className={`relative cursor-pointer z-10 inline-flex items-center ${index + 1 === page ? 'bg-indigo-600 text-white' : 'text-gray-400'} px-4 py-2 text-sm font-semibold focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
@@ -650,13 +377,13 @@ function Pagination({ page, setPage, handlePage, totalItems }) {
             )}
 
 
-            <a
-              thumbnail="#"
+            <div
+              onClick={(e) => handlePage(page + 1)}
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
           </nav>
         </div>
       </div>
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,8 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts, fetchAllProductsByFilters } from './productAPI';
+import { fetchAllProducts, fetchAllProductsByFilters, fetchCategories, fetchBrands } from './productAPI';
 
 const initialState = {
   products: [],
+  categories: [],
+  brands: [],
   status: 'idle',
   totalItems: 0,
   value: 0,
@@ -26,6 +28,25 @@ export const fetchAllProductsByFiltersAsync = createAsyncThunk(
     return response;
   }
 );
+
+export const fetchBrandsAsync = createAsyncThunk(
+  'product/fetchBrands',
+  async () => {
+    const response = await fetchBrands();
+    // The value we return becomes the `fulfilled` action payload
+    return response;
+  }
+);
+
+export const fetchCategoriesAsync = createAsyncThunk(
+  'product/fetchCategories',
+  async () => {
+    const response = await fetchCategories();
+    // The value we return becomes the `fulfilled` action payload
+    return response;
+  }
+);
+
 
 export const productSlice = createSlice({
   name: 'product',
@@ -51,6 +72,20 @@ export const productSlice = createSlice({
         state.status = 'idle';
         state.products = action.payload.data.products;
         state.totalItems = action.payload.data.totalItems;
+      })
+      .addCase(fetchBrandsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchBrandsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.brands = action.payload;
+      })
+      .addCase(fetchCategoriesAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCategoriesAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.categories = action.payload;
       });
   },
 });
@@ -58,6 +93,10 @@ export const productSlice = createSlice({
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+
+export const selectBrands = (state) => state.product.brands;
+
+export const selectCategories = (state) => state.product.categories;
 
 export const selectTotalItems = (state) => state.product.totalItems;
 
